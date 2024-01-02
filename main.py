ec2c3264d23c5f4e7aa6324606468ce07f5b6d1e
0zl <zwiefk0@gmail.com>
2024-01-02 13:48:19 +0700

    config and state

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from os import getenv
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from scripts.config import MostimaConfig
+import scripts.state as state
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
+config = MostimaConfig(getenv('CONFIG_PATH') or 'config.toml')
 
 
 app.mount('/public', StaticFiles(directory='public'), name='public')
---
+++ scripts/config.py
@@ -0,0 +1,41 @@
+import tomli, tomli_w
+import scripts.state as state
+from typing import *
+from dataclasses import dataclass
+
+
+@dataclass
+class ConfigEndpoint:
+    url: str
+    api_key: Optional[str]
+
+
+@dataclass
+class ConfigParameters:
+    temperature: float
+    max_tokens: int
+    top_p: float
+    frequency_penalty: float
+    presence_penalty: float
+
+
+@dataclass
+class Config:
+    endpoint: ConfigEndpoint
+    parameters: ConfigParameters
+
+
+class MostimaConfig:
+    config_path: str
+
+    def __init__(self, config_path: str):
+        self.config_path = config_path
+        with open(config_path, "rb") as f:
+            state.config = tomli.load(f)
+
+    def get_config(self) -> Config:
+        return state.config
+
+    def update_config(self):
+        with open(self.config_path, "wb") as f:
+            tomli_w.dump(state.config, f)
